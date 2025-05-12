38f992cd2ad5e0571e9b5a66fcb1ec2d63db3069
mrtnanastasiya <145048739+mrtnanastasiya@users.noreply.github.com>
2025-05-12 11:34:56 +0300

    task.js
    Внесены изменения в файле tooltip/task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,29 @@
 const tooltips = document.querySelectorAll('.has-tooltip');
+let activeTooltip; // Переменная для хранения ссылки на активную подсказку
 
 tooltips.forEach((tooltip) => {
-    tooltip.addEventListener('click', () => {
+    tooltip.addEventListener('click', (event) => {
+        event.preventDefault();
+
         const tooltipText = tooltip.getAttribute('title');
+        const linkDataText = tooltip.dataset.text || ''; 
 
-        const tooltipElement = document.createElement('div');
-        tooltipElement.className = 'tooltip';
-        tooltipElement.textContent = tooltipText;
-        tooltipElement.classList.add('tooltip_active');
+        if (activeTooltip) { // Если есть активная подсказка
+            if (tooltipText !== activeTooltip.textContent) { // Если текст подсказки не совпадает с текстом новой ссылки
+                activeTooltip.textContent = tooltipText; // Обновляем текст подсказки
+                activeTooltip.style.left = tooltip.getBoundingClientRect().left + 'px'; // Позиционируем подсказку относительно новой ссылки
+                activeTooltip.style.top = tooltip.getBoundingClientRect().bottom + 'px';
+            } else { // Если тексты совпадают
+                activeTooltip.classList.toggle('tooltip_active'); // Инвертируем отображение подсказки
+            }
+        } else { // Если нет активной подсказки
+            const newTooltip= `
+            <div class="tooltip tooltip_active" style="top: ${tooltip.getBoundingClientRect().bottom}px;">
+                ${tooltipText}
+            </div>`;
 
-        tooltip.insertAdjacentElement('afterend', tooltipElement);
-
-        setTimeout(() => {
-            tooltipElement.remove();
-        }, 2000);
+            tooltip.insertAdjacentHTML('afterend', newTooltip);
+            activeTooltip = tooltip.nextElementSibling;
+        }
     });
 });
